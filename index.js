d0710efe7749fef756bae0b893310a4edeabed9b
Leveque Lucile <lululeveq@hotmail.fr>
2017-12-20 20:11:48 +0100

    Début de mise en place MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+//index.js
+/* --------------------- Déclaration des imports et paramétrage général --------------------- */
+
 const express = require('express'),
   app = express(),
   http = require('http'),
   bodyParser = require("body-parser"),
   server = http.createServer(app),
   _ = require('lodash'),
-  mongoClient = require('mongodb').MongoClient,
+  MongoClient = require('mongodb').MongoClient,
   assert = require('assert');
 
 //Define the resources repository. Call "next()" instead of 404 error when file is not found.
@@ -15,11 +18,21 @@ app.use(bodyParser.json());
 //The "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded.
 app.use(bodyParser.urlencoded({ extended: true })); //Use qs library (true)
 
-// use res.render to load up an ejs view file
+/* --------------------- Gestion de la BDD --------------------- */
+
+/*var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connexion à la BDD opérationnelle.");
+  db.close();
+});*/
+
+/* --------------------- Gestion des pages --------------------- */
 
 // index page
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/index'); // use res.render to load up an ejs view file
 });
 
 /* --------------------- Lance le serveur --------------------- */

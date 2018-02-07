a317750784ace1c593ec9ebaf129781d31ecdd94
Leveque Lucile <lululeveq@hotmail.fr>
2018-02-07 18:57:52 +0100

    Début de la mise en place des comptes utilisateurs : page de connexion, d'inscription, gestion du routing selon qu'il y ait un utilisateur connecté ou non, début de lien avec mongodb.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 /* --------------------- Déclaration des imports et paramétrage général --------------------- */
 
 const express = require('express'),
-  app = express(),
   http = require('http'),
-  bodyParser = require("body-parser"),
+  app = express(),
   server = http.createServer(app),
+  expressSession = require('express-session'),
+  bodyParser = require("body-parser"),
   _ = require('lodash'),
-  MongoClient = require('mongodb').MongoClient,
+  mongoose = require('mongoose'),
+  passport = require('passport'),
   assert = require('assert');
 
 //Define the resources repository. Call "next()" instead of 404 error when file is not found.
@@ -17,23 +19,59 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 //The "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded.
 app.use(bodyParser.urlencoded({ extended: true })); //Use qs library (true)
+app.use(expressSession({secret: 'mySecretKey'})); //TODO : voir pour le paramétrage de ce truc
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* --------------------- Gestion de la BDD --------------------- */
 
-/*var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connexion à la BDD opérationnelle.");
-  db.close();
-});*/
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var Account = new Schema({
+    username: String,
+    password: String
+});
+
+Account.plugin(passportLocalMongoose);
+
 
 /* --------------------- Gestion des pages --------------------- */
 
-// index page
+//page d'accueil
 app.get('/', function(req, res) {
-    res.render('pages/index'); // use res.render to load up an ejs view file
+    res.render('pages/index', {user : req.user}); //check si connecté
+}); // use res.render to load up an ejs view file + parameters
+
+//page d'enregistrement
+app.get('/register', function(req, res) {
+    res.render('pages/register', {user : req.user});
 });
+
+app.post('register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+       if (err) { return res.render('register', { account : account }); }
+       passport.authenticate('local')(req, res, function () {
+           res.redirect('/');
+       });
+   });
+});
+
+//page de login
+app.get('/login', function(req, res) {
+    res.render('pages/login', {user : req.user});
+});
+
+app.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+//logout
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+})
+
 
 /* --------------------- Lance le serveur --------------------- */
 
@@ -42,3 +80,8 @@ app.set("port", serverPort); //Mise en place du port d'écoute
 app.listen(serverPort, function() { //Lancement du serveur
   console.log(`Votre appli' est opérationnelle au port ${serverPort} \n`);
 });
+
+
+/* --------------------- Sources et tuto utilisés --------------------- */
+//http://mherman.org/blog/2015/01/31/local-authentication-with-passport-and-express-4/#.VbkHSfYvBhE
+//https://code.tutsplus.com/tutorials/authenticating-nodejs-applications-with-passport--cms-21619

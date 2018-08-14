5a23de6dca8eb189f92eade47fba422169caf9b6
Leveque Lucile <lululeveq@hotmail.fr>
2018-08-14 23:13:45 +0200

    DataManager ajouté afin de gérer l'ajout et la modification de données en BDD

---
+++ data_manager.py
@@ -0,0 +1,25 @@
+# Gestionnaire de données (musiques, utilisateurs, etc.)
+from models import User, Music
+from App import database
+
+class DataManager():
+
+    def add_new_user(self, login, password, email):
+        user = User(login=login, email=email)
+        user.set_passwpord(password)
+        database.session.add(user)
+        database.commit()
+
+    def add_new_musique(self, title, source, duration, loop):
+        music = Music(title=title, source=source, duration=duration, loop=loop, vote=0)
+        database.session.add(music)
+        database.commit()
+        # TODO:
+        # style_tags = database.relationship('Style', secondary=music_style)
+        # scene_tags = database.relationship('Scene', secondary=music_scene)
+
+    def add_music_style_tag():
+        # TODO
+    
+    def add_music_scene_tag():
+        # TODO

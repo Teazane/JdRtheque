f002798b5aa925e6d95df9313ccdef31e8625e76
Lucile Lévêque <lululeveq@hotmail.fr>
2018-08-17 11:09:44 +0200

    Implémentation des fonctions de login

--- App.py
+++ App.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from werkzeug.urls import url_parse
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config.from_object(Config)
 database = SQLAlchemy(app)
 migrate = Migrate(app, database)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-import models
+from models import User
 from webforms import LoginForm, RegisterForm
 from data_manager import DataManager
 
@@ -18,7 +20,7 @@ from data_manager import DataManager
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', user=False)
+    return render_template('index.html')
 
 
 @app.route('/favico.ico')
@@ -28,16 +30,26 @@ def favicon():
 
 @app.route('/connexion', methods=['GET', 'POST'])
 def login():
-    # TODO : Vérifier qu'un utilisateur n'est pas déjà connecté
+    if current_user.is_authenticated:  # Si déjà connecté
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():  # Vérifie qu'on est dans le cas d'une requête POST et qu'on valide
-        return redirect(url_for('index'))
-    return render_template('login.html', user=False, form=form)
+        user = User.query.filter_by(login=form.login.data).first()
+        if user is None or not user.check_password(form.password.data):  # Si mauvais login ou mdp
+            # TODO : Voir pour une message d'erreur
+            return redirect(url_for('login'))
+        # TODO : Voir pour un message de confirmation
+        login_user(user)
+        next_page = request.args.get('next')  # Récupération de la page de redirection s'il y en avait une
+        if not next_page or url_parse(next_page).netloc != '':  # netloc doit être nul (empêche une redirection malicieuse vers un autre domaine)
+            next_page = url_for('index')  # Sinon mettre "index" comme page de retour
+        return redirect(next_page)
+    return render_template('login.html', form=form)
 
 
 @app.route('/compte')
+@login_required
 def account():
-    # TODO : Vérifier qu'un utilisateur est connecté
     return render_template('account.html', user=False)
 
 
@@ -48,19 +60,20 @@ def register():
     if form.validate_on_submit():  # Vérifie qu'on est dans le cas d'une requête POST et qu'on valide
         DataManager.add_new_user(form.login.data, form.password.data, form.email.data)
         return redirect(url_for('index'))  # TODO : décider de la page de redirection
-    return render_template('register.html', user=False, form=form)
+    return render_template('register.html', form=form)
 
 
 @app.route('/deconnexion')
+@login_required
 def logout():
-    # TODO : Vérifier qu'un utilisateur est déjà connecté
-    return render_template('index.html', user=False)
+    logout_user()
+    return render_template('index.html')
 
 
 @app.route('/rechercher_musique')
 def music_search():
     # TODO
-    return render_template('music_search.html', user=False)
+    return render_template('music_search.html')
 
 
 # ----------------- Lancement de l'appli'

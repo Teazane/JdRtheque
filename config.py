b37e65a1b2ade1cce6e8e1a383d4d70f40fdeedf
Lucile Lévêque <lucile.leveque@orange.com>
2018-11-15 14:57:59 +0100

    BDD: mise en place d'une BDD SQLite pour le dev

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # TODO: A changer quand lancement en prod'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://jdrtheque_user:jdrtheque@localhost/jdrtheque'  # TODO: A changer quand lancement en prod'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'jdrthequeDevDB.db') #SQLite pour travailler
+    #'mysql://jdrtheque_user:jdrtheque@localhost/jdrtheque'  # TODO: Mdp à changer quand lancement en prod + BDD MySQL'
     SQLALCHEMY_TRACK_MODIFICATIONS = False

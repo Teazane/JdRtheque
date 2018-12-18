0b9992e6a78f7ed56f5c531274c117028f77c6b6
Lucile Lévêque <lucile.leveque@orange.com>
2018-12-18 14:08:32 +0100

    Data_manager.py: Fonction d'ajout de styles et de scenes en BDD

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,5 @@
 # Gestionnaire de données (musiques, utilisateurs, etc.)
-from models import User, Music
+from models import User, Music, Scene, Style
 from App import database
 
 
@@ -21,10 +21,12 @@ class DataManager:
         # style_tags = database.relationship('Style', secondary=music_style)
         # scene_tags = database.relationship('Scene', secondary=music_scene)
 
-    def add_music_style_tag(self):
-        # TODO
-        return 0
+    def add_music_style_tag(self, name):
+        style = Style(name=name)
+        database.session.add(style)
+        database.session.commit()
     
-    def add_music_scene_tag(self):
-        # TODO
-        return 0
+    def add_music_scene_tag(self, name):
+        scene = Scene(name=name)
+        database.session.add(scene)
+        database.session.commit()

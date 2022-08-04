6f14288931650c576020859c27cd358f76f39f19
Lucile Lévêque <lululeveq@hotmail.fr>
2022-08-04 21:16:47 +0200

    [WIP] Scene and style add pages reintegrated

--- jdrtheque/music/urls.py
+++ jdrtheque/music/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MusicListView.as_view(), name='music-list')
+    path('', views.MusicListView.as_view(), name='music-list'),
+    path('scene/add/', views.SceneCreateView.as_view(), name='scene-add'),
+    path('style/add/', views.StyleCreateView.as_view(), name='style-add'),
 ]
--- jdrtheque/music/views.py
+++ jdrtheque/music/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from music.models import Music
+from django.views.generic.edit import CreateView
+from music.models import Music, Style, Scene
 
 
 class MusicListView(ListView):
     model = Music
     paginate_by = 100  # if pagination is desired
+
+
+class StyleCreateView(CreateView):
+    model = Style
+    fields = ['name']
+    template_name = "music/style_scene_add.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_for"] = "style"
+        return context
+
+
+class SceneCreateView(CreateView):
+    model = Scene
+    fields = ['name']
+    template_name = "music/style_scene_add.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_for"] = "scène"
+        return context

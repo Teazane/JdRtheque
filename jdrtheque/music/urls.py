280c1071ba7c2c501c7fe0d203e684a6e3f3d4da
Lucile Lévêque <lululeveq@hotmail.fr>
2022-08-04 21:32:05 +0200

    [WIP] Add music and add genre pages

--- jdrtheque/music/urls.py
+++ jdrtheque/music/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.MusicListView.as_view(), name='music-list'),
+    path('add/', views.MusicCreateView.as_view(), name='music-add'),
     path('scene/add/', views.SceneCreateView.as_view(), name='scene-add'),
     path('style/add/', views.StyleCreateView.as_view(), name='style-add'),
+    path('genre/add/', views.GenreCreateView.as_view(), name='genre-add'),
 ]
--- jdrtheque/music/views.py
+++ jdrtheque/music/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
-from music.models import Music, Style, Scene
+from music.models import Music, Style, Scene, Genre
 
 
 class MusicListView(ListView):
     model = Music
-    paginate_by = 100  # if pagination is desired
+    paginate_by = 100
+
+
+class MusicCreateView(CreateView):
+    model = Music
+    fields = [
+        'title',
+        'source',
+        'loop',
+        'styles',
+        'scenes',
+        'genres',
+    ]
 
 
 class StyleCreateView(CreateView):
     model = Style
     fields = ['name']
-    template_name = "music/style_scene_add.html"
+    template_name = "music/style_scene_genre_form.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -23,9 +35,20 @@ class StyleCreateView(CreateView):
 class SceneCreateView(CreateView):
     model = Scene
     fields = ['name']
-    template_name = "music/style_scene_add.html"
+    template_name = "music/style_scene_genre_form.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["form_for"] = "scène"
+        return context
+
+
+class GenreCreateView(CreateView):
+    model = Genre
+    fields = ['name']
+    template_name = "music/style_scene_genre_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form_for"] = "genre"
         return context

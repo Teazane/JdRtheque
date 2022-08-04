f1e5b833e4402d9f2c5b105f6a1c7b17ba2be6b4
Lucile Lévêque <lululeveq@hotmail.fr>
2022-08-04 22:58:21 +0200

    Logout redirect URL added

--- jdrtheque/jdrtheque/settings.py
+++ jdrtheque/jdrtheque/settings.py
@@ -102,3 +102,5 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'home'

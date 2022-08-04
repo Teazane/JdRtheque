987d6fd843245a10f0a226f146fe02653f38a820
Lucile Lévêque <lululeveq@hotmail.fr>
2022-08-04 16:43:33 +0200

    Home page added

--- jdrtheque/jdrtheque/urls.py
+++ jdrtheque/jdrtheque/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from jdrtheque import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('music/', include('music.urls'))
+    path('music/', include('music.urls')),
+    path('', views.HomePageView.as_view(), name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
